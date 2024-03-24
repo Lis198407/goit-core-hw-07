ec109aa01564ae57f9f518a58f3a325173fed30d
Lis <lytvinenko.i@gmail.com>
2024-03-24 17:54:21 +0200

    v1

---
+++ Autotest_module 7_1.py
@@ -0,0 +1,672 @@
+# Autotest
+#11
+# from random import randrange
+
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+#         return self.coordinates.x, self.coordinates.y
+
+#     def __add__(self, vector):
+#         x = self.coordinates.x + vector.coordinates.x
+#         y = self.coordinates.y + vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __sub__(self, vector):
+#         x = self.coordinates.x - vector.coordinates.x
+#         y = self.coordinates.y - vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __mul__(self, vector):
+#         return (
+#                 self.coordinates.x * vector.coordinates.x
+#                 + self.coordinates.y * vector.coordinates.y
+#         )
+
+#     def len(self):
+#         return (self.coordinates.x ** 2 + self.coordinates.y ** 2) ** 0.5
+
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+#     def __eq__(self, vector):
+#         return self.len() == vector.len()
+
+#     def __ne__(self, vector):
+#         return self.len() != vector.len()
+
+#     def __lt__(self, vector):
+#         return self.len() < vector.len()
+
+#     def __gt__(self, vector):
+#         return self.len() > vector.len()
+
+#     def __le__(self, vector):
+#         return self.len() <= vector.len()
+
+#     def __ge__(self, vector):
+#         return self.len() >= vector.len()
+
+
+# class Iterable:
+#     def __init__(self, max_vectors, max_points):
+#         self.current_index = 0
+#         self.vectors = []
+#         for i in range(max_vectors):
+#             self.vectors.append(Vector(Point(randrange(max_points),randrange(max_points))))
+#             # self.vectors[i].coordinates.x = randrange(max_points)
+#             # self.vectors[i].coordinates.y = randrange(max_points)
+        
+            
+
+#     def __next__(self):
+#         try:
+#             self.current_index +=1
+#             return self.vectors[self.current_index-1]
+#         except Exception:
+#             raise StopIteration
+
+    
+
+# class RandomVectors:
+    
+#     def __init__(self, max_vectors=10, max_points=50):
+#         self.max_vectors = max_vectors
+#         self.max_points = max_points
+
+#     def __iter__(self):
+#         return Iterable(self.max_vectors,self.max_points)
+   
+# vectors = RandomVectors(5, 10)
+
+# for vector in vectors:
+#     print(vector)
+
+#10
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+#         return self.coordinates.x, self.coordinates.y
+
+#     def __add__(self, vector):
+#         x = self.coordinates.x + vector.coordinates.x
+#         y = self.coordinates.y + vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __sub__(self, vector):
+#         x = self.coordinates.x - vector.coordinates.x
+#         y = self.coordinates.y - vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __mul__(self, vector):
+#         return (
+#                 self.coordinates.x * vector.coordinates.x
+#                 + self.coordinates.y * vector.coordinates.y
+#         )
+
+#     def len(self):
+#         return (self.coordinates.x ** 2 + self.coordinates.y ** 2) ** 0.5
+
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+#     def __eq__(self, vector):
+#         return self.len() == vector.len()
+
+#     def __ne__(self, vector):
+#         return self.len() != vector.len()        
+
+#     def __lt__(self, vector):
+#         return self.len() < vector.len()                
+
+#     def __gt__(self, vector):
+#         return self.len() > vector.len()                        
+
+#     def __le__(self, vector):
+#         return self.len() <= vector.len()                        
+
+#     def __ge__(self, vector):
+#         return self.len() >= vector.len()                        
+
+# vector1 = Vector(Point(1, 10))
+# vector2 = Vector(Point(3, 10))
+
+# print(vector1 == vector2)  # False
+# print(vector1 != vector2)  # True
+# print(vector1 > vector2)  # False
+# print(vector1 < vector2)  # True
+# print(vector1 >= vector2)  # False
+# print(vector1 <= vector2)  # True
+
+
+#9
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+#         return self.coordinates.x, self.coordinates.y
+
+#     def __add__(self, vector):
+#         x = self.coordinates.x + vector.coordinates.x
+#         y = self.coordinates.y + vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __sub__(self, vector):
+#         x = self.coordinates.x - vector.coordinates.x
+#         y = self.coordinates.y - vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __mul__(self, vector):
+#         return (
+#                 self.coordinates.x * vector.coordinates.x
+#                 + self.coordinates.y * vector.coordinates.y
+#         )
+
+#     def len(self):
+#         return (self.coordinates.x **2 + self.coordinates.y**2)**0.5
+
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+#8
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+#         return self.coordinates.x, self.coordinates.y
+
+#     def __add__(self, vector):
+#         x = self.coordinates.x + vector.coordinates.x
+#         y = self.coordinates.y + vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __sub__(self, vector):
+#         x = self.coordinates.x - vector.coordinates.x
+#         y = self.coordinates.y - vector.coordinates.y
+#         return Vector(Point(x, y))
+
+#     def __mul__(self, vector):
+#         return self.coordinates.x*vector.coordinates.x+self.coordinates.y*vector.coordinates.y
+
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+# vector1 = Vector(Point(1, 10))
+# vector2 = Vector(Point(10, 10))
+
+# scalar = vector2 * vector1
+
+# print(scalar)  # 110
+
+#7
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+#         return self.coordinates.x, self.coordinates.y
+
+#     def __add__(self, vector):
+#         return Vector(Point(self.coordinates.x+vector.coordinates.x,self.coordinates.y+vector.coordinates.y))
+        
+#     def __sub__(self, vector):
+#         return Vector(Point(self.coordinates.x-vector.coordinates.x,self.coordinates.y-vector.coordinates.y))
+            
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+    
+
+# vector1 = Vector(Point(1, 10))
+# vector2 = Vector(Point(10, 10))
+
+# vector3 = vector2 + vector1
+# vector4 = vector2 - vector1
+
+# print(vector3)  # Vector(11,20)
+# print(vector4)  # Vector(9,0)
+
+#6
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f"Point({self.x},{self.y})"
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __call__(self, value=None):
+#         if value != None:
+#             self.coordinates.x = self.coordinates.x * value
+#             self.coordinates.y = self.coordinates.y * value
+
+#         return (self.coordinates.x,self.coordinates.y)
+
+#     def __str__(self):
+#         return f"Vector({self.coordinates.x},{self.coordinates.y})"
+
+# vector = Vector(Point(1, 10))
+# print(vector())  # (1, 10)
+
+# vector = Vector(Point(1, 10))
+# print(vector(5))  # (5, 50)
+
+
+#5
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+#     def __str__(self):
+#         return f'Point({self.x},{self.y})'
+        
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value
+#         if index == 1:
+#             self.coordinates.y = value
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+#     def __str__(self):
+#         return f'Vector({self.coordinates.x},{self.coordinates.y})'
+
+#4
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if (type(x) == int) or (type(x) == float):
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if (type(y) == int) or (type(y) == float):
+#             self.__y = y
+
+
+# class Vector:
+#     def __init__(self, coordinates: Point):
+#         self.coordinates = coordinates
+
+#     def __setitem__(self, index, value):
+#         if index == 0:
+#             self.coordinates.x = value 
+#         if index == 1:
+#             self.coordinates.y = value 
+
+#     def __getitem__(self, index):
+#         if index == 0:
+#             return self.coordinates.x
+#         if index == 1:
+#             return self.coordinates.y
+
+# vector = Vector(Point(1, 10))
+
+# print(vector.coordinates.x)  # 1
+# print(vector.coordinates.y)  # 10
+
+# vector[0] = 10  # Встановлюємо координату x вектора 10
+
+# print(vector[0])  # 10
+# print(vector[1])  # 10
+
+
+#3
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = None
+#         self.__y = None
+#         self.x = x
+#         self.y = y
+
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @x.setter
+#     def x(self, x):
+#         if type(x) in [int, float]:
+#             self.__x = x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @y.setter
+#     def y(self, y):
+#         if type(y) in [int, float]:
+#             self.__y = y            
+
+# p = Point (1,3.4)
+# print(p.x, p.y)
+
+
+#2
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+    
+#     @property
+#     def x(self):
+#         return self.__x
+
+#     @property
+#     def y(self):
+#         return self.__y
+
+#     @x.setter
+#     def x(self,value):
+#         self.__x = value
+
+#     @y.setter
+#     def y(self,value):
+#         self.__y = value
